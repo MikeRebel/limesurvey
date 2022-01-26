e5cea7898f95a0b9f2b437c031674d3315bc5890
Mike <56391191+MikeRebel@users.noreply.github.com>
2022-01-26 12:11:05 +0300

    Buttons text change

---
+++ buttom_text/text_change.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" charset="utf-8">
+$(document).on('ready pjax:scriptcomplete',function(){
+  var lang = "{LANG}";
+  if(lang=='en') {
+  $('#ls-button-submit').text("My English Next Button");
+  $('#ls-button-previous').text("My English Back Button"); 
+  } 
+});
+</script>
