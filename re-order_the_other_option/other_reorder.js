a074635497de542bd7f44865f28a23e33d7a6048
Mike <56391191+MikeRebel@users.noreply.github.com>
2022-05-25 11:07:28 +0300

    Create other_reorder.js

---
+++ re-order_the_other_option/other_reorder.js
@@ -0,0 +1,16 @@
+/* You can simply place the “other” option on top of the list by adjusting the code above as follows: 
+Adjust these settings 
+ var move_element="other";
+ var place_before="1"; */
+
+<script type='text/javascript'>
+$(document).ready(function() {
+/* Adjust these settings */
+ var move_element="other";
+ var place_before="6";
+/* Please do NOT change the lines below */
+var place_before="javatbd{SGQ}"+place_before;
+var move_element="javatbd{SGQ}"+move_element;
+$("#"+move_element).insertBefore("#"+place_before);
+});
+</script>
